5a3d4719778c25b0142af8e9f6311d7b33226f57
Eitaro Fukamachi <e.arrows@gmail.com>
2018-06-02 11:15:58 +0900

    Rename db-fetch -> fetch in db module.

--- modules/db.lisp
+++ modules/db.lisp
@@ -2,12 +2,12 @@
   (:use #:cl)
   (:import-from #:cl-dbi)
   (:export #:*db*
-           #:db-fetch))
+           #:fetch))
 (in-package #:paras/modules/db)
 
 (defvar *db*)
 
-(defun db-fetch (sql &optional binds)
+(defun fetch (sql &optional binds)
   (second
    (dbi:fetch
     (apply #'dbi:execute (dbi:prepare *db* sql) binds))))
