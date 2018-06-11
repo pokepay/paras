f2ded916d378e348def79bb1f28a10d5eab83ae4
Eitaro Fukamachi <e.arrows@gmail.com>
2018-06-11 16:24:24 +0900

    Add 'modules/db:execute'.

--- modules/db.lisp
+++ modules/db.lisp
@@ -2,7 +2,8 @@
   (:use #:cl)
   (:import-from #:cl-dbi)
   (:export #:*db*
-           #:fetch))
+           #:fetch
+           #:execute))
 (in-package #:paras/modules/db)
 
 (defvar *db*)
@@ -11,3 +12,6 @@
   (second
    (dbi:fetch
     (apply #'dbi:execute (dbi:prepare *db* sql) binds))))
+
+(defun execute (sql &optional binds)
+  (apply #'dbi:do-sql *db* sql binds))
