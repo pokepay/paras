a5d9b5c93594c3a65806d945183b21ec81d0cb56
Eitaro Fukamachi <e.arrows@gmail.com>
2021-11-12 01:29:51 +0000

    Add 'fetch-all' to db module.

--- modules/db.lisp
+++ modules/db.lisp
@@ -3,6 +3,7 @@
   (:import-from #:cl-dbi)
   (:export #:*db*
            #:fetch
+           #:fetch-all
            #:execute))
 (in-package #:paras/modules/db)
 
@@ -12,6 +13,10 @@
   (second
    (dbi:fetch
     (dbi:execute (dbi:prepare *db* sql) binds))))
+
+(defun fetch-all (sql &optional binds)
+  (dbi:fetch-all
+    (dbi:execute (dbi:prepare *db* sql) binds)))
 
 (defun execute (sql &optional binds)
   (dbi:do-sql *db* sql binds))
